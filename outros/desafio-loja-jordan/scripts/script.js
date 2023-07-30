3e1ccbf15242cf8b5a04c1f9eac0e6673af9fe2c
nathalia.alves <nathalia.alves@sankhya.com.br>
2023-07-30 00:11:37 -0300

    Desafio 02 - Codelândia

---
+++ outros/desafio-loja-jordan/scripts/script.js
@@ -0,0 +1,5 @@
+// Typed.js
+var typed = new Typed('.animation', {
+    strings: ["O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
+    typeSpeed: 45,
+});
